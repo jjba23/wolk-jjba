c61036fb88e046a84a6b5f6fa917991b171856b5
Josep Bigorra <jjbigorra@gmail.com>
2024-10-25 14:18:44 +0200

    Fine words and an insinuating appearance are seldom associated with true virtue.

--- config.scm
+++ config.scm
@@ -35,6 +35,7 @@
              (gnu packages compression)
              (gnu packages rust-apps)
              (gnu packages avahi)
+             (gnu services avahi)
              (gnu packages linux)
              (gnu packages sqlite)
              (gnu packages curl)

ae743f9afa50f4575d5e613556fe01f4eba7a726
Josep Bigorra <jjbigorra@gmail.com>
2024-10-26 16:13:41 +0200

    Fine words and an insinuating appearance are seldom associated with true virtue.

---
+++ home/joe/home.scm
@@ -0,0 +1,39 @@
+;;; home.scm
+
+;; Copyright (C) 2024 Josep Jesus Bigorra Algaba
+
+;; wolk-jjba is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; wolk-jjba is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with wolk-jjba.  If not, see <https://www.gnu.org/licenses/>.
+
+(use-modules (gnu home)
+             (gnu home services)
+             (gnu home services shells)
+             (gnu packages emacs)
+             (gnu packages admin)
+             (gnu services)
+             (guix gexp))
+(use-modules (gnu home services ssh)
+             (gnu home services gnupg)
+             (gnu packages gnupg))
+(use-modules (gnu home services sound)
+             (gnu home services desktop) )
+
+(display "\n>>= configuring home environment...\n")
+(home-environment
+ (packages
+  (list pfetch)) 
+ (services
+  (list 
+   (service home-dbus-service-type)
+   
+   )))
